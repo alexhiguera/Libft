deba245b4b1577905c873d50b12b0b093ac7fd0e
alexhiguera <ahiguera@student.42urduliz.com>
2023-09-20 16:52:48 +0200

    build (/libft): creo ft_toupper.c y funciona

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,14 +6,54 @@
 /*   By: ahiguera <ahiguera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 15:29:59 by ahiguera          #+#    #+#             */
-/*   Updated: 2023/09/18 15:46:08 by ahiguera         ###   ########.fr       */
+/*   Updated: 2023/09/20 15:34:14 by ahiguera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <string.h>
 
-size_t	strlcat(char *dst, const char *src, size_t dstsize)
+int	ft_strlen(char *str)
 {
-	
+	int		i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t			i;
+	size_t			j;
+	unsigned int	dst_len;
+
+	i = ft_strlen(dst);
+	dst_len = ft_strlen(dst);
+	j = 0;
+	if (dstsize && ft_strlen(dst) <= dstsize)
+	{
+		while (src[j] != '0' && i < (dstsize - 1))
+		{
+			dst[i] = src[j];
+			i++;
+			j++;
+		}
+	}
+	if (dst[i] == '\0')
+		dst_len = 0;
+	if (dstsize < dst_len)
+		dst_len = dstsize;
+	return (dst_len + ft_strlen((char *)src));
+}
+
+int	main(void)
+{
+	char	source;
+	char	destiny;
+
+	source = "funciona";
+	printf("frasecilla: %s \n resultado: %s \n", source, destiny);
+	return (0);
 }
---
+++ libft/ft_toupper.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_toupper.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahiguera <ahiguera@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/20 15:36:24 by ahiguera          #+#    #+#             */
+/*   Updated: 2023/09/20 16:50:45 by ahiguera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* #include <stdio.h> */
+
+int	ft_toupper(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		c -= 32;
+	return (c);
+}
+
+/* int	main(void)
+{
+	printf("resultado: %i", ft_toupper('a'));
+	return (0);
+} */
+
+/* La función toupper() convierte una letra en minúscula en
+la letra correspondiente en mayúscula. El argumento debe
+ser representable como un carácter sin signo o tener el
+valor de EOF (End of File). */
