5a938928ee1709c73d76e12ad82d7cd47f0ab65c
alexhiguera <ahiguera@student.42urduliz.com>
2023-09-27 14:48:59 +0200

    update (ft_strncmp): elimino comentarios, elimino strlen, llamo a strlen y comprobaciones de main

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -1,14 +1,14 @@
-
-
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahiguera <ahiguera@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/26 21:34:17 by ahiguera          #+#    #+#             */
+/*   Updated: 2023/09/27 14:45:54 by ahiguera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 int	strncmp(const char *s1, const char *s2, size_t n)
 {
@@ -21,31 +21,3 @@ int	strncmp(const char *s1, const char *s2, size_t n)
 	second = ft_strlen(s2);
 	return (first - second);
 }
-
-/*#include <string.h>
-#include <stdio.h>
-
- int	main(void)
-{
-	int	primero;
-	int	segundo;
-	char	*a;
-	char	*s;
-	int	 resultado;
-
-	a = "holadfgdfg";
-	s = "hola";
-	primero = ft_strlen(a);
-	segundo = ft_strlen(s);
-	resultado = primero - segundo;
-	printf("1 es: %s y 2 es: %s , entonces resultado es: %i", a, s, resultado);
-	return (0);
-} */
-
-
-/* Compara dos cadenas de caracteres y determinar si son iguales o cuál es
-lexicográficamente mayor. Devuelve un valor entero que indica la relación:
-
-if == 0 las cadenas son idénticas.
-if < 0 la 1 cadena es menor que la 2.
-if > 0 la 1 cadena ese mayor que la 2. */
