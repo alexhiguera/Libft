6d7a37e3b4eceb0408e4f0b285b7a66b69254876
alexhiguera <ahiguera@student.42urduliz.com>
2023-09-28 14:06:07 +0200

    update (libft.h): llamo a las 19 funciones creadas hasta el momento

--- libft/libft.h
+++ libft/libft.h
@@ -6,13 +6,33 @@
 /*   By: ahiguera <ahiguera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 21:11:51 by ahiguera          #+#    #+#             */
-/*   Updated: 2023/09/26 21:13:27 by ahiguera         ###   ########.fr       */
+/*   Updated: 2023/09/28 14:03:57 by ahiguera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-int	ft_tolower(int c);
+#include <stdlib.h>
+
+int		ft_atoi(const char *str);
+void	ft_bzero(void *s, size_t n);
+int		ft_isalnum(char c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+void	*ft_memchr(const void *s, int c, int n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memset(void *s, int c, size_t len);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcpy(char *dest, const char *src, size_t destsize);
+int		ft_strlen(char *str);
+int		strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strrchr(const char *s, int c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
 
 #endif
