8e53199817d06bf5751c70bda1e4d9fe18169538
alexhiguera <ahiguera@student.42urduliz.com>
2023-10-02 17:05:10 +0200

    build (libft): creo calloc, compruebo y funciona

---
+++ libft/ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahiguera <ahiguera@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/02 16:59:05 by ahiguera          #+#    #+#             */
+/*   Updated: 2023/10/02 17:04:03 by ahiguera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*result;
+
+	result = malloc(count * size);
+	if (result == NULL)
+		return (NULL);
+	ft_bzero(result, count * size);
+	return (result);
+}
