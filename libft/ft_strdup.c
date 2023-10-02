8ab037209db70bddb0cdcaca56bf3b824f74bedc
alexhiguera <ahiguera@student.42urduliz.com>
2023-10-02 18:17:32 +0200

    build (libft): creo función ft_strdup prto no me funciona, lo dejo como borrador

---
+++ libft/ft_strdup.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahiguera <ahiguera@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/02 17:05:41 by ahiguera          #+#    #+#             */
+/*   Updated: 2023/10/02 18:16:15 by ahiguera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+	char	*result;
+	int		len;
+	int		i;
+
+	i = 0;
+	len = ft_strlen(s1);
+	result = malloc((len + 1);
+	if (result == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		result[i] = s1[i];
+		i++;
+	}
+	return (result);
+}
