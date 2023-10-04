29c36390ddc09b57cf92ff88e8be748e1140a24d
alexhiguera <ahiguera@student.42urduliz.com>
2023-10-04 16:48:30 +0200

    update (ft_toupper): elimino main

--- libft/ft_toupper.c
+++ libft/ft_toupper.c
@@ -6,7 +6,7 @@
 /*   By: ahiguera <ahiguera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 15:36:24 by ahiguera          #+#    #+#             */
-/*   Updated: 2023/10/02 18:31:42 by ahiguera         ###   ########.fr       */
+/*   Updated: 2023/10/04 16:47:19 by ahiguera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,4 @@ int	ft_toupper(int c)
 	if (c >= 'a' && c <= 'z')
 		c -= 32;
 	return (c);
-}
-
-int	main(void)
-{
-	printf("resultado: %i", ft_toupper(98));
-	return (0);
 }
