e7f6ac6eb488ad1c4ea3b3ca5dca3d097fc2d6ad
alexhiguera <ahiguera@student.42urduliz.com>
2023-10-06 17:23:00 +0200

    fix (ft_strlcpy): corrijo los valores '\0'

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: ahiguera <ahiguera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 13:59:13 by ahiguera          #+#    #+#             */
-/*   Updated: 2023/10/04 14:41:46 by ahiguera         ###   ########.fr       */
+/*   Updated: 2023/10/06 17:15:59 by ahiguera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t destsize)
 	size_t	i;
 
 	i = 0;
-	if (destsize != 0)
+	if (destsize != '\0')
 	{
-		while (src[i] != '0' && i < (destsize - 1))
+		while (src[i] != '\0' && i < (destsize - 1))
 		{
 			dest[i] = src[i];
 			i++;
