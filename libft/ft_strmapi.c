7d2bd1d4596a4c1fada9a11cab04be1a6a4b6b1f
alexhiguera <ahiguera@student.42urduliz.com>
2023-10-06 17:18:47 +0200

    fix (ft_strmapi): corrijo errores de norma y añado cabecera

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -1,22 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahiguera <ahiguera@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/06 16:53:36 by ahiguera          #+#    #+#             */
+/*   Updated: 2023/10/06 16:55:02 by ahiguera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strmapi(const char *s, char (*f)(unsigned int, char))
+char	*ft_strmapi(const char *s, char (*f)(unsigned int, char))
 {
-    unsigned int	i;
-    char			*result;
+	unsigned int	i;
+	char			*result;
 	size_t			len;
 
 	len = ft_strlen(s);
-    result = malloc((len + 1) * sizeof(char));
-    if (!result)
-        return (NULL);
-    i = 0;
-    while (i < len)
-    {
-        result[i] = (*f)(i, s[i]);
-        i++;
-    }
-    result[i] = 0;
-    return (result);
+	result = malloc((len + 1) * sizeof(char));
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		result[i] = (*f)(i, s[i]);
+		i++;
+	}
+	result[i] = 0;
+	return (result);
 }
