1ac1519fb230e8992f4a074067bbe8e5127767e5
alexhiguera <ahiguera@student.42urduliz.com>
2023-10-06 17:17:40 +0200

    fix (ft_strjoin): corrijo errores de norma y añado cabecera

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -1,4 +1,14 @@
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahiguera <ahiguera@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/06 16:52:54 by ahiguera          #+#    #+#             */
+/*   Updated: 2023/10/06 16:53:03 by ahiguera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
@@ -10,7 +20,6 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	i = 0;
 	j = 0;
-
 	result = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
 	if (!result)
 		return (NULL);
