73c39f2de3e952c5c3de76088c73f3e9cabec43a
alexhiguera <ahiguera@student.42urduliz.com>
2023-10-06 13:20:36 +0200

    fix (ft_memcmp): cambio vars a unsigned char y cambio lugar de retorno

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: ahiguera <ahiguera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 16:10:40 by ahiguera          #+#    #+#             */
-/*   Updated: 2023/10/03 18:18:38 by ahiguera         ###   ########.fr       */
+/*   Updated: 2023/10/06 13:18:30 by ahiguera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	char	*str1;
-	char	*str2;
-	int		i;
+	unsigned char	*str1;
+	unsigned char	*str2;
+	size_t			i;
 
 	i = 0;
 	str1 = (unsigned char *)s1;
@@ -28,6 +28,6 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 			return (str1[i] - str2[i]);
 			i++;
 		}
-		return (0);
 	}
+	return (0);
 }
