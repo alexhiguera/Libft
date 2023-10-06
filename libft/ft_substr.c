46099829c2c1ebd316f12e5e6f30656a9e42671e
alexhiguera <ahiguera@student.42urduliz.com>
2023-10-06 13:33:26 +0200

    build (libft): creo función ft_substr. pdte testear

---
+++ libft/ft_substr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahiguera <ahiguera@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/02 19:33:02 by ahiguera          #+#    #+#             */
+/*   Updated: 2023/10/03 18:31:32 by ahiguera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	*substr;
+	int		i;
+
+	i = start;
+	len = ft_strlen(s);
+	substr = malloc(len + 1);
+	while ()
+	{
+	}
+	return (substr);
+}
+
+//null
