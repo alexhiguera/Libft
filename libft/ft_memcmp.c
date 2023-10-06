d5813a7371440493b3c1b5b5511124c056995d5f
alexhiguera <ahiguera@student.42urduliz.com>
2023-10-06 12:58:27 +0200

    build (libft): creo función ft_memcmp. pdte comprovación

---
+++ libft/ft_memcmp.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahiguera <ahiguera@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/03 16:10:40 by ahiguera          #+#    #+#             */
+/*   Updated: 2023/10/03 18:18:38 by ahiguera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	char	*str1;
+	char	*str2;
+	int		i;
+
+	i = 0;
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	while (i < n)
+	{
+		if (str1[i] != str2[i])
+		{
+			return (str1[i] - str2[i]);
+			i++;
+		}
+		return (0);
+	}
+}
