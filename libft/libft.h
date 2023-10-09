e4aa9d973d7bc8de53bf824dd0f9c2fb8930aa7f
alexhiguera <ahiguera@student.42urduliz.com>
2023-10-09 20:52:08 +0200

    update (libft.h): añado más funciones de bonus

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ahiguera <ahiguera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 21:11:51 by ahiguera          #+#    #+#             */
-/*   Updated: 2023/10/09 18:16:00 by ahiguera         ###   ########.fr       */
+/*   Updated: 2023/10/09 20:39:10 by ahiguera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 
 typedef struct s_list
 {
-	void			*contet;
+	void			*content;
 	struct s_list	*next;
 }					t_list;
 
@@ -58,5 +58,7 @@ t_list	ft_lstnew(void	*content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	lstdelone(t_list *lst, void (*del)(void *));
 
 #endif
