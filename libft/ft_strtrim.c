eda1be261f14e347600a39e1418cdb7b810a70f4
alexhiguera <ahiguera@student.42urduliz.com>
2023-10-16 14:17:01 +0200

    update (ft_strtrim): cambio nombre de funcion static y reescribo bucle null

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,13 +6,13 @@
 /*   By: ahiguera <ahiguera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 16:50:45 by ahiguera          #+#    #+#             */
-/*   Updated: 2023/10/06 17:27:14 by ahiguera         ###   ########.fr       */
+/*   Updated: 2023/10/16 13:49:39 by ahiguera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static	int	to_trim(const char *set, char c)
+static int	is_in_set(char c, const char *set)
 {
 	int	i;
 
@@ -34,17 +34,17 @@ char	*ft_strtrim(char const *s1, char const *set)
 	size_t	end;
 
 	start = 0;
-	while (s1[start] && to_trim(s1[start], set))
+	while (s1[start] && is_in_set(s1[start], set))
 		start++;
 	end = ft_strlen(s1);
-	while (end > start && to_trim(s1[end - 1], set))
+	while (end > start && is_in_set(s1[end - 1], set))
 		end--;
-	str = (char *)malloc(sizeof(*s1) * (end - start + 1));
-	if (!str)
+	str = (char *)malloc(sizeof(char) * (end - start + 1));
+	if (str == NULL)
 		return (NULL);
 	i = 0;
 	while (start < end)
 		str[i++] = s1[start++];
-	str[i] = 0;
+	str[i] = '\0';
 	return (str);
 }
