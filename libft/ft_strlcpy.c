003735e49ac2dbe1a96d274d524e19b25a26e66a
alexhiguera <ahiguera@student.42urduliz.com>
2023-10-18 20:28:34 +0200

    fix (ft_strlcpy): agrego cambios para casos especiales

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: ahiguera <ahiguera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 13:59:13 by ahiguera          #+#    #+#             */
-/*   Updated: 2023/10/06 17:15:59 by ahiguera         ###   ########.fr       */
+/*   Updated: 2023/10/18 19:04:35 by ahiguera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,18 @@
 size_t	ft_strlcpy(char *dest, const char *src, size_t destsize)
 {
 	size_t	i;
+	size_t	src_len;
 
+	src_len = ft_strlen(src);
 	i = 0;
-	if (destsize != '\0')
+	if (destsize != 0)
 	{
-		while (src[i] != '\0' && i < (destsize - 1))
+		while (i < src_len && i < (destsize - 1))
 		{
 			dest[i] = src[i];
 			i++;
 		}
 		dest[i] = '\0';
 	}
-	return (i);
+	return (src_len);
 }
