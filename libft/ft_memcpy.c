55ff4f9c94786d74c41292ef89eea9bfab228a73
alexhiguera <ahiguera@student.42urduliz.com>
2023-10-18 11:45:38 +0200

    fix (ft_memcpy): cambio nombres de variables y sus prototipos, modifico el buucle por i<n

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: ahiguera <ahiguera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 18:27:01 by ahiguera          #+#    #+#             */
-/*   Updated: 2023/10/02 18:21:53 by ahiguera         ###   ########.fr       */
+/*   Updated: 2023/10/16 15:28:21 by ahiguera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	unsigned int	i;
-	const char		*origen;
-	char			*dest;
+	size_t	i;
+	char	*origin;
+	char	*dest;
 
 	i = 0;
-	origen = (char *)src;
-	dest = dst;
-	while (i < n && (src || dst))
+	origin = (char *)src;
+	dest = (char *)dst;
+	while (i < n)
 	{
-		dest[i] = origen[i];
+		dest[i] = origin[i];
 		i++;
 	}
 	return (dst);
