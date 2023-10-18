5fe3618349436813c392da20cc7d7047b9d5132a
alexhiguera <ahiguera@student.42urduliz.com>
2023-10-18 20:22:40 +0200

    fix (ft_substr): corrijo codigo para evaluar casos especiales

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: ahiguera <ahiguera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 19:33:02 by ahiguera          #+#    #+#             */
-/*   Updated: 2023/10/16 14:01:19 by ahiguera         ###   ########.fr       */
+/*   Updated: 2023/10/18 15:33:55 by ahiguera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,19 @@
 char	*ft_substr(char const *str, unsigned int start, size_t len)
 {
 	char	*substr;
-	size_t	i;
-	size_t	max_len;
+	size_t	str_len;
 
-	max_len = ft_strlen(str);
-	if (max_len < start)
-		max_len -= start;
-	i = 0;
+	if (str == NULL)
+		return (NULL);
+	str_len = ft_strlen(str);
+	if (str_len <= start)
+		len = 0;
+	else if (str_len <= start + len)
+		len = str_len - start;
 	substr = (char *)malloc((len + 1) * sizeof (char));
 	if (substr == NULL)
 		return (NULL);
-	while (str[i] != '\0' && (i > start && max_len < len))
-		return (0);
-	substr[i] = 0;
+	ft_memcpy(substr, str + start, len);
+	substr[len] = '\0';
 	return (substr);
 }
