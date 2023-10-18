22c701f52d89ff7f8f39ff28df2be3bea2be9388
alexhiguera <ahiguera@student.42urduliz.com>
2023-10-18 11:47:16 +0200

    fix (ft_memmove): reconstruyo función creando una static inversa a a memcpy

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,25 +6,36 @@
 /*   By: ahiguera <ahiguera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 12:20:41 by ahiguera          #+#    #+#             */
-/*   Updated: 2023/10/04 14:32:56 by ahiguera         ###   ########.fr       */
+/*   Updated: 2023/10/16 15:55:35 by ahiguera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dest, const void *src, size_t n)
+static void	*ft_reversecopy(void *dst, const void *src, size_t n)
 {
 	size_t	i;
-	char	*destiny;
-	char	*orin;
+	char	*origin;
+	char	*dest;
 
-	i = 0;
-	destiny = (char *)dest;
-	orin = (char *)src;
-	while (i < n)
+	origin = (char *)src;
+	dest = (char *)dst;
+	i = n - 1;
+	while (i + 1 > 0)
 	{
-		destiny[i] = orin[i];
-		i++;
+		dest[i] = origin[i];
+		i--;
 	}
-	return (destiny);
+	return (dst);
+}
+
+void	*ft_memmove(void *dst, const void *src, size_t n)
+{
+	ssize_t	no;
+
+	no = (ssize_t)n;
+	if (dst - src > 0 && dst - src < no)
+		return (ft_reversecopy(dst, src, n));
+	else
+		return (ft_memcpy(dst, src, n));
 }
