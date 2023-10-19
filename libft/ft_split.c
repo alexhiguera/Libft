b60532a078da0f2766f26729401e493d37ed7f8b
alexhiguera <ahiguera@student.42urduliz.com>
2023-10-19 15:59:33 +0200

    update (ft_split): refactorizo eliminando 1 funcion de más

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: ahiguera <ahiguera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 12:46:24 by ahiguera          #+#    #+#             */
-/*   Updated: 2023/10/16 10:46:52 by ahiguera         ###   ########.fr       */
+/*   Updated: 2023/10/18 15:46:30 by ahiguera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,57 +28,59 @@ static size_t	counter(char const *str, char c)
 	return (total);
 }
 
-static char	*copy_word(char *str, char c, size_t *i)
+static char	*copy_word(char const *str, char c, size_t *i)
 {
 	char	*result;
 	size_t	len;
 
-	while (str[*i] == c)
+	while (str[*i] != '\0' && str[*i] == c)
 		(*i)++;
 	len = 0;
-	while (str[*i] != '\0' && str[*i + len] != c)
+	while (str[*i + len] != '\0' && str[*i + len] != c)
 		len++;
 	result = ft_substr(str, *i, len);
 	*i += len;
 	return (result);
 }
 
-static void	separate(char *str, char **split, char c)
-{
-	size_t	i;
-	size_t	j;
-
-	i = 0;
-	j = 0;
-	while (str[i] != '\0')
-	{
-		split[j] = copy_word(str, c, &i);
-		while (str[i] == c)
-			i++;
-	}
-}
-
-static void	free(char **clear)
+static void	ft_free(char **split)
 {
 	size_t	i;
 
 	i = 0;
-	while (clear[i])
+	while (split[i])
 	{	
-		free (clear[i]);
+		free(split[i]);
 		i++;
 	}
-	free (clear);
+	free(split);
 }
 
 char	**ft_split(char const *str, char c)
 {
 	char	**result;
 	size_t	num_words;
+	size_t	i;
+	size_t	j;
 
+	if (str == NULL)
+		return (NULL);
 	num_words = counter(str, c);
 	result = (char **)malloc(sizeof(char *) * (num_words + 1));
-	separate(str, result, c);
+	if (result == NULL)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (j < num_words)
+	{
+		result[j] = copy_word(str, c, &i);
+		if (result[j] == NULL)
+		{
+			ft_free(result);
+			return (NULL);
+		}
+		j++;
+	}
 	result[num_words] = NULL;
 	return (result);
 }
