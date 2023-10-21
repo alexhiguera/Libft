a0a3e43bbad8553a0ca9d14d84d7502798732e04
alexhiguera <ahiguera@student.42urduliz.com>
2023-10-21 13:16:45 +0200

    fix (ft_lstmap): cambio nombre de variable set -> content y cambi retornos a null

--- libft/ft_lstmap_bonus.c
+++ libft/ft_lstmap_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ahiguera <ahiguera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:57:47 by ahiguera          #+#    #+#             */
-/*   Updated: 2023/10/19 16:12:35 by ahiguera         ###   ########.fr       */
+/*   Updated: 2023/10/21 12:43:31 by ahiguera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,23 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_node;
 	t_list	*result_list;
-	void	*set;
+	void	*content;
 
 	if (lst == NULL || f == NULL || del == NULL)
 		return (NULL);
 	new_node = NULL;
-	while (lst)
+	while (lst != NULL)
 	{
-		set = f(lst->content);
-		new_node = ft_lstnew(set);
-		if (new_node == NULL)
+		content = f(lst->content);
+		result_list = ft_lstnew(content);
+		if (result_list == NULL)
 		{
-			del(set);
-			ft_lstclear(&result_list, (*del));
-			return (result_list);
+			del(content);
+			ft_lstclear(&new_node, del);
+			return (NULL);
 		}
-		ft_lstadd_back(&result_list, new_node);
+		ft_lstadd_back(&new_node, result_list);
 		lst = lst->next;
 	}
-	return (result_list);
+	return (new_node);
 }
