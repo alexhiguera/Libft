5f5c9ef6108b279aeb4a6d4a07fe30fe8028ed2b
alexhiguera <ahiguera@student.42urduliz.com>
2023-10-21 13:11:47 +0200

    fix (ft_lstadd_back): cambio retorno null a void y evaluo en null el if

--- libft/ft_lstadd_back_bonus.c
+++ libft/ft_lstadd_back_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ahiguera <ahiguera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 20:02:13 by ahiguera          #+#    #+#             */
-/*   Updated: 2023/10/09 20:21:01 by ahiguera         ###   ########.fr       */
+/*   Updated: 2023/10/20 15:44:30 by ahiguera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	t_list	*one_more;
 
 	one_more = ft_lstlast(*lst);
-	if (!one_more)
+	if (one_more == NULL)
+	{
 		*lst = new;
+		return ;
+	}
 	one_more -> next = new;
 }
